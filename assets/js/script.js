8b3f3f735e9eb381d7647882678b4d0a270ae4f4
LifeInvader <matthewkausfox@hotmail.com>
2021-05-12 21:25:55 -0600

    added Javascript which includes API, and form call

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,71 @@
-$(document).ready(function (){
+$(document).ready(function () {
+
+    // API key
+    var apiKey = "17d8a2c1551299169e5ab5c406cb1540"
+
+    // DOM elements
+    var cityEl = $("#city-input");
+    var searchFieldEl = $("#search-field");
+    var submitEl = $("#sbmt-btn")
+
+    searchFieldEl.submit(function (event) {
+        event.preventDefault();
+
+        // get value from input
+        var cityVal = cityEl.val();
+        console.log(cityVal);
+
+        if (cityEl) {
+            getWeather(cityVal);
+        } else {
+            alert("Please enter a city")
+        }
+
+
+    });
+
+
+
+
+
+
+
+
+
+    // var iconcode = a.weather[0].icon;
+    // var iconurl = "http://openweathermap.org/img/w/" + iconcode + ".png";
+    // $('#wicon').attr('src', iconurl);
+
+    var getWeather = function (city) {
+        fetch("https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=17d8a2c1551299169e5ab5c406cb1540")
+            .then(function (response) {
+                response.json()
+                    .then(function (data) {
+                        console.log(data)
+                        generateCurrentWeath(data)
+                    })
+            });
+    };
+
+    // getWeather();
+
+
+    function generateCurrentWeath(data) {
+        // var icon = $('<img src="https://openweathermap.org/img/wn/" +"01d" + ".png"></img>')
+        $(".panel-heading").text(data.name)
+        // $(".field").append(icon)
+
+        var iconcode = data.weather[0].icon;
+        var iconurl = "http://openweathermap.org/img/w/" + iconcode + ".png"
+        var icon = $("<img>")
+        icon.attr("src", iconurl)
+
+        // $(".field").append(icon)
+
+
+        console.log(iconcode);
+        console.log
+    }
 
 
 
@@ -29,13 +96,4 @@ $(document).ready(function (){
 
 
 
-
-
-
-
-
-
-
-
-    
 })
